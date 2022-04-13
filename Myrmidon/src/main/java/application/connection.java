13987cbbe9dfc3e1cdbe60b4c3203b4f83010837
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-04-14 00:45:29 +0200

    Making a general outline for the basic needs of each class.

---
+++ Myrmidon/src/main/java/application/connection.java
@@ -0,0 +1,12 @@
+package application;
+
+public class connection {
+
+    public void getConnection (){
+        // connect to Database
+    }
+
+    public void removeConnection(){
+        // end connection
+    }
+}
---
+++ Myrmidon/src/main/java/tests/testStatistics.java
@@ -0,0 +1,24 @@
+package tests;
+
+public class testStatistics {
+
+    public void testDisplayChartToday(){
+        // displays the days statistics
+    }
+
+    public void testDisplayChartOneWeek(){
+        // displays the weeks statistics
+    }
+
+    public void testDisplayChartOneMonth(){
+        // displays the months statistics
+    }
+
+    public void testDisplayChartOneYear(){
+        // displays all statistics from the calendar year
+    }
+
+    public void testDisplayJournalEntry(){
+        // displays the desired journal entry
+    }
+}
