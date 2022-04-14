ff634a131af1ef7d825a6f162db610bfd1a7dfcb
Froschi1860 <fabianf96@googlemail.com>
2022-04-14 13:40:39 +0200

    fix: added maven test structure, added sql scripts, added DatabaseConnection class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.myrmidon {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
     opens com.example.myrmidon to javafx.fxml;
     exports com.example.myrmidon;
