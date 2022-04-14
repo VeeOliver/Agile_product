b8321d0830acc855bdccdab382a78077dfea1779
enzotiberghien <enzotiberghien28@gmail.com>
2022-04-14 12:02:09 +0200

    first commit

---
+++ src/main/java/com/example/myrmidon/User.java
@@ -0,0 +1,10 @@
+package com.example.myrmidon;
+
+public class User {
+    String email, password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
