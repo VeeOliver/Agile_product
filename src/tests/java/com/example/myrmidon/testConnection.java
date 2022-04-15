d71f5131e460872a24785aa0340170403f542724
Froschi1860 <fabianf96@googlemail.com>
2022-04-15 13:20:51 +0200

    fix: updated filestructure

---
+++ src/tests/java/com/example/myrmidon/testConnection.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class testConnection {
+
+    public void testGetConnection (){
+        // able to connect to Database
+    }
+
+    public void testRemoveConnection(){
+        // able to end the connection
+    }
+}
---
+++ src/tests/java/com/example/myrmidon/testGUI.java
@@ -0,0 +1,5 @@
+package tests;
+
+public class testGUI {
+    // to be updated
+}
