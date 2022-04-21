f3bb4762c6a1314dcd703c034ab76d34d0514ca2
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-04-21 12:22:09 +0200

    Setting framework methods for the data class

--- src/main/java/se/hkr/app/Data.java
+++ src/main/java/se/hkr/app/Data.java
@@ -4,5 +4,23 @@ import java.util.ArrayList;
 
 
 public class Data {
-    static public ArrayList<User> users = new ArrayList<>();
-}
+    //static public ArrayList<User> users = new ArrayList<>();
+
+    public static void insertMood(String moodRating, User user){
+        int moodNum = Integer.parseInt(moodRating);
+
+    }
+
+    public static void insertTension(String tensionRating, User user){
+        int tensionNum = Integer.parseInt(tensionRating);
+
+    }
+
+    public static void insertJournal(String journalText, User user){
+
+
+    }
+
+
+} 
+
