0dba924d7c09154f88cf30e334aca4b41fad5fe9
Fabian Fröschl <fabianf96@googlemail.com>
2022-04-22 10:55:45 +0200

    Setting up integration test framework

---
+++ src/it/java/ExampleIT.java
@@ -0,0 +1,13 @@
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleIT {
+    @Test
+    void testIntegrationTest() {
+        int res = 0;
+        assertEquals(0, res);
+    }    
+}
