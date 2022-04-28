478e81566c11a24bccea805824cb9b5f61ea792f
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-04-29 00:43:52 +0200

    adding an icon for the app NOTE: the tinyicon must stay in the Agile Development package folder, outside of the img folder or it will not work

--- src/main/java/se/hkr/app/Welcome.java
+++ src/main/java/se/hkr/app/Welcome.java
@@ -3,6 +3,7 @@ package se.hkr.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,8 @@ public class Welcome extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         String css = this.getClass().getResource("welcome.css").toExternalForm();
         scene.getStylesheets().add(css);
+        Image appIcon = new Image("File:tinyicon.png");
+        stage.getIcons().add(appIcon);
         stage.setTitle("Myrmidon");
         stage.setScene(scene);
         stage.setResizable(false);
