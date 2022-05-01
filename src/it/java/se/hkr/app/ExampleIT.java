942926d99e192b082eced288609d5a87178a5121
Froschi1860 <fabianf96@googlemail.com>
2022-05-01 15:30:11 +0200

    finalized docker setup and sanitized pom.xml

--- src/it/java/se/hkr/app/ExampleIT.java
+++ src/it/java/se/hkr/app/ExampleIT.java
@@ -1,43 +1,22 @@
 package se.hkr.app;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.junit.jupiter.api.Test;
-
 public class ExampleIT {
-    /*@Test
-    public void testIntegrationTest() {
-        int res = 0;
-        assertEquals(0, res);
-    }
-    @Test
-    public void testIntegrationTest2() {
-        int res = 0;
-        assertEquals(0, res);
-    }*/
+    /*
+    For integration tests a database is set up which is always in the same state at
+    the start of the test run. It containes the sample data defined in 
+    /sql/sample_data_insert.ddl which can be used to validate own method output.
 
+    When doing integration tests against this containerized database, give the method
+    DatabaseConnection.getInstance() the String "127.0.0.1:5000" as an argument.
+    Then call the connect() function on the DatabaseConnnection object to build the
+    connection to the database in the container and use this for whichever query
+    you want to execute.
+    
+    Example code to build connection for integration tests:
     @Test
     public void tryConnectionToDocker() {
-        //DatabaseConnection dbCon = DatabaseConnection.getInstance("127.0.0.1:5000");
-
-        //assertEquals("jdbc:mariadb://localhost:5000/myrmidon?user=myrmidon_admin&password=myr_ADM123", dbCon.getDsn());
-        Connection con = null;
-        try {
-            String url = "jdbc:mariadb://127.0.0.1:5000/myrmidon?user=myrmidon_admin&password=myr_ADM123";
-            con = DriverManager.getConnection(url);
-            System.out.println("success");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        DatabaseConnection dbCon = DatabaseConnection.getInstance("127.0.0.1:5000");
+        Connection con = dbCon.connect();
         assertNotNull(con);
-    }
+    }*/
 }
