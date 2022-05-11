b1e89399ac3b4e1a2743627d6d39289c52546890
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-05-11 19:58:49 +0200

    created an about us page

--- src/main/java/se/hkr/app/WelcomeController.java
+++ src/main/java/se/hkr/app/WelcomeController.java
@@ -31,6 +31,14 @@ public class WelcomeController {
     @FXML
     Button loginBtn;
 
+    @FXML
+    Button aboutBtn;
+
+    @FXML
+    Button backBtn;
+
+
+
 
 
     Authentication auth = new Authentication();
@@ -89,6 +97,16 @@ public class WelcomeController {
         factOfTheDay.setText(fact);
 
     }
+
+    // link to about us page
+    public void showAboutUs(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("about-us.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
 
 
